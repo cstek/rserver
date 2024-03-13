d18f351ea99483dad3c394e71c2365b91d824e68
qcloud <ubuntu@localhost.localdomain>
2024-03-13 10:44:58 +0800

    configure on webserver

--- rserver/settings.py
+++ rserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4t0wyhrr)=!d_h6z&6t*v&hu3)8v&v&4bdly%d0nb$uxi&qaz1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['esp32rfid.top', 'www.esp32rfid.top', '43.128.27.152', '127.0.0.1']
 
 
 # Application definition
