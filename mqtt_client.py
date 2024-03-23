78b11409d7e2677256097c089596e9fd962fbe64
z01001 <z01001@outlook.com>
2024-03-23 19:09:58 +0800

    change mqtt_client.py

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,31 +1,11 @@
 # mqtt_client.py
 import paho.mqtt.client as mqtt
+import time
 
 # MQTT服务器设置
-MQTT_HOST = 'localhost'
+MQTT_HOST = '43.128.27.152'
 MQTT_PORT = 1883
 MQTT_TOPIC = 'fromEsp'
-#
-#
-# # 当接收到MQTT消息时的回调函数
-# def on_message(client, userdata, message):
-#     print(f"Received message '{message.payload.decode()}' on topic '{message.topic}'")
-#
-#
-# # 创建MQTT客户端实例
-# client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-#
-# # 指定回调函数
-# client.on_message = on_message
-#
-# # 连接到MQTT服务器
-# client.connect(MQTT_HOST, MQTT_PORT, 60)
-#
-# # 订阅主题
-# client.subscribe(MQTT_TOPIC)
-#
-# # 开始监听订阅的主题（在后台线程中）
-# client.loop_start()
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -38,7 +18,7 @@ def on_connect(client, userdata, flags, reason_code, properties):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print("LOCAL_TIME: " + str(time.time()) + msg.topic+" "+str(msg.payload))
 
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
