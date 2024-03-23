dcf4d98b32938c2cf6c12407c8d15da14a5e9b24
z01001 <z01001@outlook.com>
2024-03-23 18:41:56 +0800

    change

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,10 +1,10 @@
 # mqtt_client.py
-# import paho.mqtt.client as mqtt
-#
-# # MQTT服务器设置
-# MQTT_HOST = 'localhost'
-# MQTT_PORT = 1883
-# MQTT_TOPIC = 'fromEsp'
+import paho.mqtt.client as mqtt
+
+# MQTT服务器设置
+MQTT_HOST = 'localhost'
+MQTT_PORT = 1883
+MQTT_TOPIC = 'fromEsp'
 #
 #
 # # 当接收到MQTT消息时的回调函数
@@ -26,5 +26,29 @@
 #
 # # 开始监听订阅的主题（在后台线程中）
 # client.loop_start()
-for i in range(10):
-    print(i)
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(MQTT_TOPIC)
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+
+mqttc.connect(MQTT_HOST, 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+mqttc.loop_forever()
