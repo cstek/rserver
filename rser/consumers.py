a0cc4845ace8ae16773a6fe33ebde022669b891c
z01001 <z01001@outlook.com>
2024-03-25 01:54:08 +0800

    test change

--- rser/consumers.py
+++ rser/consumers.py
@@ -6,32 +6,34 @@ import paho.mqtt.client as mqtt
 
 
 class MyConsumer(AsyncWebsocketConsumer):
-    mqtt_client = None  # 定义MQTT客户端为类变量
-
-    async def connect(self):
-        await self.accept()
-
-        # 使用async_to_sync包装器调用同步方法
-        if not MyConsumer.mqtt_client:
-            async_to_sync(self.setup_mqtt_client)()
-
-    def setup_mqtt_client(self):
-        def on_message(client, userdata, message):
-            # 当MQTT接收到消息时，将其发送到WebSocket
-            try:
-                # 注意，这里使用了async_to_sync包装器
-                async_to_sync(self.send)(text_data=json.dumps({'message': message.payload.decode()}))
-            except Exception as e:
-                print(f"Error sending message: {e}")
-
-        MyConsumer.mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-        MyConsumer.mqtt_client.on_message = on_message
-        MyConsumer.mqtt_client.connect("localhost", 1883, 60)
-        MyConsumer.mqtt_client.subscribe("fromEsp")
-        MyConsumer.mqtt_client.loop_forever()  # 使用loop_start而不是loop_forever
-
-    async def disconnect(self, close_code):
-        if MyConsumer.mqtt_client:
-            MyConsumer.mqtt_client.loop_stop()  # 停止MQTT客户端循环
-            MyConsumer.mqtt_client.disconnect()  # 断开MQTT客户端连接
-            MyConsumer.mqtt_client = None
+    def connect(self):
+        async_to_sync(self.send)(text_data=json.dumps({'message': 'aaaaaaaaabbbbbbbbbbbb'}))
+    # mqtt_client = None  # 定义MQTT客户端为类变量
+    #
+    # async def connect(self):
+    #     await self.accept()
+    #
+    #     # 使用async_to_sync包装器调用同步方法
+    #     if not MyConsumer.mqtt_client:
+    #         async_to_sync(self.setup_mqtt_client)()
+    #
+    # def setup_mqtt_client(self):
+    #     def on_message(client, userdata, message):
+    #         # 当MQTT接收到消息时，将其发送到WebSocket
+    #         try:
+    #             # 注意，这里使用了async_to_sync包装器
+    #             async_to_sync(self.send)(text_data=json.dumps({'message': message.payload.decode()}))
+    #         except Exception as e:
+    #             print(f"Error sending message: {e}")
+    #
+    #     MyConsumer.mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    #     MyConsumer.mqtt_client.on_message = on_message
+    #     MyConsumer.mqtt_client.connect("localhost", 1883, 60)
+    #     MyConsumer.mqtt_client.subscribe("fromEsp")
+    #     MyConsumer.mqtt_client.loop_forever()  # 使用loop_start而不是loop_forever
+    #
+    # async def disconnect(self, close_code):
+    #     if MyConsumer.mqtt_client:
+    #         MyConsumer.mqtt_client.loop_stop()  # 停止MQTT客户端循环
+    #         MyConsumer.mqtt_client.disconnect()  # 断开MQTT客户端连接
+    #         MyConsumer.mqtt_client = None
